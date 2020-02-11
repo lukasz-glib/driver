b21f2d23bbd42c07dfefb2621052541c9a8ce759
Lukasz Glib <lukasz.gl28@gmail.com>
2020-02-11 20:41:51 +0100

    fix swagger configuration

--- src/main/java/pl/lg/DriverApplication.java
+++ src/main/java/pl/lg/DriverApplication.java
@@ -2,14 +2,11 @@ package pl.lg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class DriverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DriverApplication.class, args);
     }
-
 }
